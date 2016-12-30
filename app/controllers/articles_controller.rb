cd90418add8ef1208a4d88317316f21add3db0bf
Rails UDY course <vinay.punwani@gmail.com>
2016-12-30 22:26:56 +0000

    Add flash message styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     
     if @article.save
-      flash[:notice] = "Article was successfully created!"
+      flash[:success] = "Article #{@article.id} was successfully created!"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated!"
+      flash[:info] = "Article #{@article.id} was successfully updated!"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article #{@article.id} was successfully deleted!"
+    flash[:warning] = "Article #{@article.id} was successfully deleted!"
     redirect_to articles_path
   end
   
