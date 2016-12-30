7972a18f3033147e1675aa7ab08c968b515a36f9
Rails UDY course <vinay.punwani@gmail.com>
2016-12-30 04:39:10 +0000

    Modify routes file to make articles index

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home'
+  # root 'pages#home'
+    root 'articles#index'
+
   get 'pages/about', to: 'pages#about'
-  
   
   resources :articles
 
